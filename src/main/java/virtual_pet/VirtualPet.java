f1fa8cd9c6fd6811af9346b22c7f0db1dc3fbae4
JGibson37 <Jason.Gibson15@yahoo.com>
2020-05-15 12:18:15 -0400

    Requested push

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,4 +1,5 @@
 package virtual_pet;
 
 public class VirtualPet {
-}
+
+    }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,21 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
         //Interact with a VirtualPet object in this method
+        Scanner input = new Scanner(System.in);
+        boolean gameRun = true;
+        while (gameRun = true){
+            System.out.println("What would you like to do with your pet?");
+            int getAction=input.nextInt();
+            if (getAction == 1){
+                gameRun = false;
+                break;
+            }
+        }
     }
 
 }
