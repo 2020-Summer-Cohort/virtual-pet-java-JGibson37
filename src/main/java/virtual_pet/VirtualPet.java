b8da545ed8285ed4166a6a6c7c9992e63e3caab4
JGibson37 <Jason.Gibson15@yahoo.com>
2020-05-16 11:12:18 -0400

    One interaction 'play' working

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,5 +1,47 @@
 package virtual_pet;
 
 public class VirtualPet {
+    private int desireToPlay = 40;
+    private int desireToEat = 20;
+    private int desireToDrink = 30;
+    private int desireToSleep = 5;
+
+    public void tick() {
+        desireToPlay = desireToPlay + 7;
+        desireToEat = desireToEat + 9;
+        desireToDrink = desireToDrink + 3;
+        desireToSleep = desireToSleep + 6;
 
     }
+
+    public int play() {
+        desireToPlay = desireToPlay - 15;
+        return desireToPlay;
+    }
+
+    public int getHunger() {
+    return desireToEat;
+    }
+
+    public int getThirst() {
+        return desireToDrink;
+
+    }
+
+    void getBoredom() {
+
+    }
+
+    void getPlay() {
+
+    }
+
+   public int getDesireToPlay() {
+        return desireToPlay;
+    }
+
+    public int getTiredness() {
+        return desireToSleep;
+    }
+}
+
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,21 +1,38 @@
 package virtual_pet;
 
 import java.util.Scanner;
-
+/* Create main method that
+    1.Implements a GameLoop   DONE
+    2.Asks for user input     DONE
+    3.Writes Outputs in the console
+ */
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
         //Interact with a VirtualPet object in this method
         Scanner input = new Scanner(System.in);
+        VirtualPet virtualPet = new VirtualPet();
         boolean gameRun = true;
-        while (gameRun = true){
-            System.out.println("What would you like to do with your pet?");
-            int getAction=input.nextInt();
-            if (getAction == 1){
+        while (gameRun) {
+
+            System.out.println("What would you like your Alicorn to do?");
+            System.out.println("1. Play:" + " Desire Level: " + virtualPet.getDesireToPlay());
+            System.out.println("2. Eat:" + " Hunger Level: " + virtualPet.getHunger());
+            System.out.println("3. Drink:" + " Thirst Level: " + virtualPet.getThirst());
+            System.out.println("4. Rest: " + " Tiredness Level: " + virtualPet.getTiredness());
+
+            int getAction = input.nextInt();
+            if (getAction == 0) {
                 gameRun = false;
                 break;
+            } else if (getAction == 1) {
+                virtualPet.play();
+                System.out.println("You played with Alicorn!" + " Her desire to play is lowered.");
+
+            } else if (getAction == 2) {
+                System.out.println("Your Alicorn ate her favorite carrots. She is less hungry.");
             }
+            virtualPet.tick();
         }
     }
-
 }
